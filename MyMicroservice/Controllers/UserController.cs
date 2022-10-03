ddc83864edf26353e96fb1eee95b4eb42a039d59
Nikolay Zhelyazkov <NZZhelyazkov@nemetschek.bg>
2022-10-03 14:47:30 +0300

    Initial Basic API Web App

---
+++ MyMicroservice/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMicroservice.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyMicroservice.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly User[] _users = new[]
+        {
+            new User { Username = "JDN", Name = "John Doe", Age = 42 },
+            new User { Username = "JDNW", Name = "Jane Doe", Age = 39 },
+            new User { Username = "SD", Name = "Sammy Doe", Age = 13 },
+        };
+
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public List<User> GetAllUsers()
+        {
+            return this._users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public User GetUser(int id)
+        {
+            if (id > this._users.Length - 1)
+            {
+                return new User
+                {
+                    Username = "Username",
+                    Name = "User name",
+                    Age = 0
+                };
+            }
+
+            return this._users[id];
+        }
+    }
+
+}
---
+++ MyMicroservice/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyMicroservice.Models
+{
+    public class User
+    {
+        [Required]
+        public string? Username { get; set; }
+        [Required]
+        public string? Name { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
