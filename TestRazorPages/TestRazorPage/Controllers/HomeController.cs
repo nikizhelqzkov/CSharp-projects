a9df971ab79558e7d5b834415fdddefcfea237a1
Nikolay Zhelyazkov <NZZhelyazkov@nemetschek.bg>
2023-04-11 10:54:54 +0300

    add new test mvc project for rise testing

---
+++ TestRazorPages/TestRazorPage/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestRazorPage.Models;
+
+namespace TestRazorPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Test()
+        {
+            return View(new List<DataViewModel>()
+            {
+                new DataViewModel { Id = 0, Name = "test", Age = 13 },
+                new DataViewModel { Id = 1, Name = "test2", Age = 12}
+            });
+        }
+    }
+}
---
+++ TestRazorPages/TestRazorPage/Models/Data.cs
@@ -0,0 +1,9 @@
+﻿namespace TestRazorPage.Models
+{
+    public class DataViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
